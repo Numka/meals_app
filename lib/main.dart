93fcaa0f0d77b2f53a11642841d7f9ad6d201dd8
Numinor <numinor93@mail.ru>
2020-11-24 21:25:11 +0300

    Added new route for meals and styled the page for them

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@
 
 import 'package:flutter/material.dart';
 
-import 'screens/category_meals_screen.dart';
-import 'screens/categories_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,6 +39,15 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return;
+        //return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
