5f2a099fad19c627c49fdac9887481465802cece
Numinor <numinor93@mail.ru>
2020-11-24 19:13:29 +0300

    Module about multiple screens, I tend to forget adding learning repos to git

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+//import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals app',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home: CategoriesScreen(),
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
+    );
+  }
+}
